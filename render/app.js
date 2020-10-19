547b364b8bbd97189000975c95255313164c1a01
canji53 <canji.preengineer@gmail.com>
2020-10-19 20:57:50 +0900

    :+1: 不要なため削除

--- render/app.js
+++ render/app.js
@@ -9,14 +9,11 @@ const app = express()
 const awsServerlessExpress = require('aws-serverless-express')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 
-const srcDir = 'nuxt-app'
-
 app.use(awsServerlessExpressMiddleware.eventContext())
 app.use(
   '/_nuxt',
   express.static(path.join(__dirname, '.nuxt', 'dist', 'client'))
 )
-app.use('/static', express.static(path.join(__dirname, srcDir, 'static')))
 
 async function start() {
   const nuxt = await loadNuxt('start')
